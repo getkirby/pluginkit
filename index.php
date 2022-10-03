d2cce6443a82f6d9740540728c368d6ea03a9157
Lukas Bestle <mail@lukasbestle.com>
2022-10-03 17:48:41 +0200

    Update indentation to tabs

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 use Kirby\Filesystem\F;
 
 F::loadClasses([
-    'superwoman\\superplugin\\superclass' => 'src/Superclass.php'
+	'superwoman\\superplugin\\superclass' => 'src/Superclass.php'
 ], __DIR__);
 
 Kirby::plugin('getkirby/pluginkit', [
-    // plugin magic happens here
+	// plugin magic happens here
 ]);
